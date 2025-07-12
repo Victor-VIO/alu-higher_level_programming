38ae098d0cbace498e84e5ddbf61d1dec26085a9
Victor Idowu <v.idowu@alustudent.com>
2025-07-12 09:07:21 -0700

    commit

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """Print all integers of a list"""
     for num in my_list:
         print("{:d}".format(num))
+
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
