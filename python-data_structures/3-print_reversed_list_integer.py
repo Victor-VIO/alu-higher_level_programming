55f51437e9925e534e20c1fbdb12dcf6b0fe1440
Victor Idowu <v.idowu@alustudent.com>
2025-07-12 09:51:29 -0700

    commit

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Prints list in reverse"""
+    if my_list is None:
+        return
     for num in reversed(my_list):
         print("{:d}".format(num))
 
@@ -8,3 +10,4 @@ def print_reversed_list_integer(my_list=[]):
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     print_reversed_list_integer(my_list)
+    print_reversed_list_integer(None)  # Test None case
