bda02fc23aa22242f2fb46d7778c9064f6307262
Victor Idowu <v.idowu@alustudent.com>
2025-07-12 09:16:05 -0700

    commit

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """Retrieves an element from a list"""
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
+
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
